bf0085369cd3b9afaede1f9cbd445874da63b569
skipperoooo <cbstevens98@gmail.com>
2022-11-16 12:16:20 -0800

    Add file

--- UBC/UBCReservations.py
+++ UBC/UBCReservations.py
@@ -1,5 +1,5 @@
 #! python3
-# UBCReservations.py - Reserves time slots at the UBC aquatic center.
+# UBCReservations.py - Reserves time slots at the UBC aquatic center using the Selenium web driver.
 import time
 import pandas as pd
 import numpy as np
@@ -9,8 +9,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
-from bs4 import BeautifulSoup
 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -34,70 +32,84 @@ password = input("Enter your CWL password:")"""
 username = 'colton04'
 password = 'DvC6SiS58ryQ!'
 
-
+# Event names from the website
 eventList = ['25m Length Swim', 'Leisure Pool','25m Length Toonie Swim UBC Staff/Faculty',
              '25m Length Swim UBC Student Access','50m Length Swim','50m Length Swim UBC Student Access',
              '50m Length Swim Member/Student Access','25m Length Swim Member/Student Access',
              '50m Length Toonie Swim UBC Staff/Faculty']
 
+# Locate and click the login button
 loginButton = driver.find_element('xpath','//*[@id="topNav"]/div[1]/a')
 loginButton.click()
 
+# Locate and click the CWL button
 CWLButton = driver.find_element('xpath','/html/body/div[1]/div[6]/div/div/div/div[2]/div[2]/div[1]/div/div[1]/p[4]/a/img')
 CWLButton.click()
 
-
+# Locate the username box and send the username
 usernameBox = driver.find_element('id', 'username')
-
 usernameBox.send_keys(username)
 
+# Locate the password box and enter the password
 passwordBox = driver.find_element('xpath','//*[@id="password"]')
 passwordBox.send_keys(password)
 passwordBox.send_keys(Keys.ENTER)
 
+# Store the dates available for reservations
 dates = WebDriverWait(driver,timeout=30).until(lambda d: d.find_elements('xpath','//h2[contains(text(),"2022")]'))
 
+# Store the classes available for reservations
 table = WebDriverWait(driver,timeout=30).until(lambda d: d.find_element('xpath','//*[@id="classes"]'))
 df = pd.read_html(str(table.get_attribute('outerHTML')))
 df = df[0]
 
+# Add the available classes to a list
 y=[]
 for i in range(len(df.values)):
     y.append(df.values[i][0])
 
+# Convert the list to a pandas series
 df_classes = pd.Series(y)
 
+# Store the names of each class in a list
 classes = []
 for i in range(len(df_classes)):
     if df_classes[i][0].isdigit():
         classes.append(df_classes[i])
     else:
-        if len(df_classes[i].split('|'))>2:
+        if len(df_classes[i].split('|')) > 2:
             classes.append(df_classes[i].split('|')[0] + '| ' + df_classes[i].split('|')[2].split(' Aquatic Centre')[0].strip())
         else:
             classes.append(df_classes[i].split('|')[0] + '| ' + df_classes[i].split('|')[1].split(' Aquatic Centre')[0].strip())
 
+# Convert the list of class names to a pandas dataframe
 classes = pd.Series(classes)
 classes = pd.DataFrame({"text":classes})
+
+# Create a column for class type based on the number of characters in the string. Fill missing class types with the type from the previous row.
 classes['type'] = np.where(classes['text'].apply(len)>14,"Event","Date")
 classes = classes.pivot(columns='type').fillna(method='ffill')
 classes.dropna(inplace=True)
 classes.columns = classes.columns.get_level_values(1)
 
+# Create columns for time, event, and category based on the regular patterns in the list of strings.
 classes['Time'] = classes['Event'].str[-19::]
 classes['Event'] = classes['Event'].str[:-19].str.strip()
 classes[['Category','Event']] = classes['Event'].str.split('|',expand=True)
 classes['Category'] = classes['Category'].str.strip()
 classes['Event'] = classes['Event'].str.strip()
 
+# Filter to classes in the list of events
 classesFiltered = classes[(classes['Event'].isin(eventList)) & (classes['Category']=="REGISTER")]
 eventsGrouped = classesFiltered.groupby(['Date','Event'])
 
+# Filter to classes in the next week
 classesFilteredNextWeek = classesFiltered[classesFiltered.Date.isin(classesFiltered.Date.drop_duplicates()[0:7])]
 
 print("Events available in the next week:")
 print(classesFilteredNextWeek[['Date', 'Event', 'Time']].to_string())
 
+# Ask the user to input a date
 while True:
     dateSelected = input("Select a date (e.g.,2022 Oct 7th):")
     if dateSelected in classesFilteredNextWeek.Date.drop_duplicates().values:
@@ -105,10 +117,12 @@ while True:
     else:
         print(f"Please enter one of the following dates: {classesFilteredNextWeek.Date.drop_duplicates().values}")
 
+# Filter classes to the inputted date
 classesFilteredDay = classesFilteredNextWeek[classesFilteredNextWeek.Date==dateSelected]
 print("Events available in the chosen day:")
 print(classesFilteredDay[['Date', 'Event', 'Time']].to_string())
 
+# Ask the user to input a time
 while True:
     timeSelected = input("Select a time (e.g., 08:30 am):")
     if timeSelected in classesFilteredDay.Time.str[0:8].values:
@@ -122,18 +136,11 @@ timesInNextWeek = classesFilteredNextWeek[classesFilteredNextWeek.Time.str[0:8]=
 
 timeIndex = timesInNextWeek[timesInNextWeek.Date==dateSelected].index[0]
 
-"""html = driver.page_source
-soup = BeautifulSoup(html,'html.parser')
-table = soup.select_one("table[id='classes']")
-table_df = pd.read_html(str(table))[0]
-print(table_df)
-print(table_df.columns)"""
-
-
-
+# Select the class with the inputted time
 a = WebDriverWait(driver,timeout=30).until(lambda d: d.find_elements('xpath',f"//*[contains(text(),'{timeSelected} -')]/../../../div[3]/div[2]/input"))
 a[timeIndex].click()
 
+# Locate and click the register button
 registerButton = WebDriverWait(driver,timeout=30).until(lambda d: d.find_element('xpath','//*[@id="main-content"]/div/div/div[1]/div/section[2]/a'))
 registerButton.click()
 
@@ -148,9 +155,11 @@ else:
 
 time.sleep(1)
 
+# Locate and click the attendees button
 nextAttendeesButton = WebDriverWait(driver,timeout=30).until(lambda d: d.find_element('xpath','//*[@id="event-attendees"]/div[3]/div/a'))
 nextAttendeesButton.click()
 
+# Locate and click the questionnaire button
 nextQuestionnaireButton = WebDriverWait(driver,timeout=30).until(lambda d: d.find_element('xpath','//*[@id="main-content"]/div/div/div[2]/div[2]/a'))
 nextQuestionnaireButton.click()
 
